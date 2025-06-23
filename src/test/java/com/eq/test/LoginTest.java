c7a8040fe9aa6ce36ca77e650b1b759f93ae5713
balaji-githubstore <balajidinakaran1@gmail.com>
2025-06-23 12:48:04 +0530

    Added DataProvider Concept

--- src/test/java/com/eq/test/LoginTest.java
+++ src/test/java/com/eq/test/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.eq.base.AutomationWrapper;
+import com.eq.utils.DataUtils;
 
 public class LoginTest extends AutomationWrapper {
 	@Test
@@ -19,16 +20,15 @@ public class LoginTest extends AutomationWrapper {
 		Assert.assertEquals(actualValue, "Time at Work");
 	}
 	
-	@Test
-	public void invalidLoginTest()
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "invalidLoginData")
+	public void invalidLoginTest(String username,String password,String expectedError)
 	{
-		driver.findElement(By.name("username")).sendKeys("john");
-		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-		
-		//Assert the error - Invalid credentials
-		String actualValue=driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
-		Assert.assertTrue(actualValue.contains("Invalid credential"),"Expected Value not matching Actual value "+actualValue); //expect true
+
+		String actualError=driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, expectedError);
 	}
 }
 
---
+++ src/test/java/com/eq/utils/DataUtils.java
@@ -0,0 +1,22 @@
+package com.eq.utils;
+
+import org.testng.annotations.DataProvider;
+
+public class DataUtils {
+	
+	@DataProvider
+	public Object[][] invalidLoginData()
+	{
+		Object[][] data=new Object[2][3];
+		
+		data[0][0]="saul";
+		data[0][1]="saul123";
+		data[0][2]="Invalid credentials";
+		
+		data[1][0]="kim";
+		data[1][1]="kim123";
+		data[1][2]="Invalid credentials";
+		
+		return data;
+	}
+}
