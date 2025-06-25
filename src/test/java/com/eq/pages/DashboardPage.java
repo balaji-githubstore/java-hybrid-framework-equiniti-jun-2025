0d72a2fa61d8db9f518248ac626c78b7e9a5142a
balaji-githubstore <balajidinakaran1@gmail.com>
2025-06-25 10:41:03 +0530

    Updated Page Object

--- src/test/java/com/eq/pages/DashboardPage.java
+++ src/test/java/com/eq/pages/DashboardPage.java
@@ -1,13 +1,21 @@
 package com.eq.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DashboardPage {
 
 	private WebDriver driver;
+	
+	private By timeAtWorkLocator=By.xpath("//p[contains(normalize-space(),'at Work')]");
 
 	public DashboardPage(WebDriver driver) {
 		this.driver = driver;
+	}
+	
+	public String getTimeAtWorkHeader()
+	{
+		return driver.findElement(timeAtWorkLocator).getText();
 	}
 
 }
