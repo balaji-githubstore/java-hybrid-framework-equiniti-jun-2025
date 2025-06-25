ea64f81223e4feb9b69a3e5f63b96e3da2684c6d
balaji-githubstore <balajidinakaran1@gmail.com>
2025-06-25 10:43:22 +0530

    LoginTest Updated

--- src/test/java/com/eq/test/LoginTest.java
+++ src/test/java/com/eq/test/LoginTest.java
@@ -5,31 +5,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.eq.base.AutomationWrapper;
+import com.eq.pages.DashboardPage;
 import com.eq.pages.LoginPage;
 import com.eq.utils.DataUtils;
 
 public class LoginTest extends AutomationWrapper {
 	@Test(dataProviderClass = DataUtils.class, dataProvider = "commonDataProvider")
 	public void validLoginTest(String username, String password, String expectedValue) {
-		
+
 		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
 		login.clickOnLogin();
 
 		// Assert the text Time at Work
-		String actualValue = driver.findElement(By.xpath("//p[contains(normalize-space(),'at Work')]")).getText();
+		DashboardPage dashboard = new DashboardPage(driver);
+		String actualValue = dashboard.getTimeAtWorkHeader();
 		Assert.assertEquals(actualValue, expectedValue);
 	}
 
 	@Test(dataProviderClass = DataUtils.class, dataProvider = "commonDataProvider")
 	public void invalidLoginTest(String username, String password, String expectedError) {
-		
+
 		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
 		login.clickOnLogin();
-		
+
 		String actualError = login.getInvalidErrorMessage();
 		Assert.assertEquals(actualError, expectedError);
 	}
